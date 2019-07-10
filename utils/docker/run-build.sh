89ececde1592e49abac13f98217c5690278bb306
Anna Marcinkowska <anna.marcinkowska@intel.com>
2019-07-10 09:51:31 +0200

    Add travis, java

---
+++ utils/docker/run-build.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+#
+# Copyright 2019, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#
+#     * Neither the name of the copyright holder nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#
+# run-build.sh - checks bindings' building and installation with pmemkv
+# 0.8 or current master
+#
+
+PREFIX=/usr
+
+set -e
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
+
+# build and install pmemkv
+cd ~
+git clone https://github.com/pmem/pmemkv.git
+cd pmemkv
+git checkout $1
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+	-DCMAKE_INSTALL_PREFIX=$PREFIX
+make -j2
+echo $USERPASS | sudo -S make install
+
+echo
+echo "#########################################################################"
+echo "### Verifying building and installing of the pmemkv-jni and java bindings"
+echo "#########################################################################"
+cd ~
+git clone https://github.com/pmem/pmemkv-jni.git
+cd pmemkv-jni
+# # copy Googletest to the current directory
+# cp /opt/googletest/googletest-*.zip .
+make test
+echo $USERPASS | sudo -S make install prefix=$PREFIX
+
+cd ~
+git clone https://github.com/pmem/pmemkv-java.git
+cd pmemkv-java
+mvn install
+
+echo
+echo "################################################################"
+echo "### Executing test(s) from pmemkv-tools"
+echo "################################################################"
+cd ~
+git clone https://github.com/pmem/pmemkv-tools.git
+cd pmemkv-tools
+make example_java
