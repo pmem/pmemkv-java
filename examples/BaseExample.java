e2fca6a47bdf749a64e35d831b2b64d4ca7322c1
Anna Marcinkowska <anna.marcinkowska@intel.com>
2019-07-18 15:32:31 +0200

    Move example from the pmemkv-tools

---
+++ examples/BaseExample.java
@@ -0,0 +1,27 @@
+import io.pmem.pmemkv.Database;
+
+public class BaseExample {
+    public static void main(String[] args) {
+        System.out.println("Starting engine");
+        Database db = new Database("vsmap", "{\"path\":\"/dev/shm\", \"size\":1073741824}");
+
+        System.out.println("Putting new key");
+        db.put("key1", "value1");
+        assert db.countAll() == 1;
+
+        System.out.println("Reading key back");
+        assert db.get("key1").equals("value1");
+
+        System.out.println("Iterating existing keys");
+        db.put("key2", "value2");
+        db.put("key3", "value3");
+        db.getKeys((String k) -> System.out.println("  visited: " + k));
+
+        System.out.println("Removing existing key");
+        db.remove("key1");
+        assert !db.exists("key1");
+
+        System.out.println("Stopping engine");
+        db.stop();
+    }
+}
---
+++ examples/README.md
@@ -0,0 +1,1 @@
+This directory contains an example for pmemkv-java.
