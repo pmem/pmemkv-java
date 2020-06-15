d24b498f288d23f11f997de07a68974672bad015
Pawel Karczewski <pawel.karczewski@intel.com>
2020-06-15 15:30:29 +0200

    Remove bytes and string API
    API with such many overloads is hard to maintain, and not needed.
    It's pretty simple to convert byte[] or String to non-direct ByteBuffer, which
    seems to cover removed functionalities.

--- src/main/java/io/pmem/pmemkv/Database.java
+++ src/main/java/io/pmem/pmemkv/Database.java
@@ -30,25 +30,9 @@ public class Database {
                 -> callback.process((ByteBuffer) k.rewind().limit(kb)));
     }
 
-    public void getKeys(GetKeysByteArraysCallback callback) {
-        database_get_keys_bytes(pointer, callback);
-    }
-
-    public void getKeys(GetKeysStringsCallback callback) {
-        database_get_keys_string(pointer, callback);
-    }
-
     public void getKeysAbove(ByteBuffer key, GetKeysBuffersCallback callback) {
         database_get_keys_above_buffer(pointer, key.position(), key, (int kb, ByteBuffer k)
                 -> callback.process((ByteBuffer) k.rewind().limit(kb)));
-    }
-
-    public void getKeysAbove(byte[] key, GetKeysByteArraysCallback callback) {
-        database_get_keys_above_bytes(pointer, key, callback);
-    }
-
-    public void getKeysAbove(String key, GetKeysStringsCallback callback) {
-        database_get_keys_above_string(pointer, key.getBytes(), callback);
     }
 
     public void getKeysBelow(ByteBuffer key, GetKeysBuffersCallback callback) {
@@ -56,25 +40,9 @@ public class Database {
                 -> callback.process((ByteBuffer) k.rewind().limit(kb)));
     }
 
-    public void getKeysBelow(byte[] key, GetKeysByteArraysCallback callback) {
-        database_get_keys_below_bytes(pointer, key, callback);
-    }
-
-    public void getKeysBelow(String key, GetKeysStringsCallback callback) {
-        database_get_keys_below_string(pointer, key.getBytes(), callback);
-    }
-
     public void getKeysBetween(ByteBuffer key1, ByteBuffer key2, GetKeysBuffersCallback callback) {
         database_get_keys_between_buffer(pointer, key1.position(), key1, key2.position(), key2, (int kb, ByteBuffer k)
                 -> callback.process((ByteBuffer) k.rewind().limit(kb)));
-    }
-
-    public void getKeysBetween(byte[] key1, byte[] key2, GetKeysByteArraysCallback callback) {
-        database_get_keys_between_bytes(pointer, key1, key2, callback);
-    }
-
-    public void getKeysBetween(String key1, String key2, GetKeysStringsCallback callback) {
-        database_get_keys_between_string(pointer, key1.getBytes(), key2.getBytes(), callback);
     }
 
     public long countAll() {
@@ -85,36 +53,12 @@ public class Database {
         return database_count_above_buffer(pointer, key.position(), key);
     }
 
-    public long countAbove(byte[] key) {
-        return database_count_above_bytes(pointer, key);
-    }
-
-    public long countAbove(String key) {
-        return database_count_above_bytes(pointer, key.getBytes());
-    }
-
     public long countBelow(ByteBuffer key) {
         return database_count_below_buffer(pointer, key.position(), key);
     }
 
-    public long countBelow(byte[] key) {
-        return database_count_below_bytes(pointer, key);
-    }
-
-    public long countBelow(String key) {
-        return database_count_below_bytes(pointer, key.getBytes());
-    }
-
     public long countBetween(ByteBuffer key1, ByteBuffer key2) {
         return database_count_between_buffer(pointer, key1.position(), key1, key2.position(), key2);
-    }
-
-    public long countBetween(byte[] key1, byte[] key2) {
-        return database_count_between_bytes(pointer, key1, key2);
-    }
-
-    public long countBetween(String key1, String key2) {
-        return database_count_between_bytes(pointer, key1.getBytes(), key2.getBytes());
     }
 
     public void getAll(GetAllBufferCallback callback) {
@@ -122,25 +66,9 @@ public class Database {
                 -> callback.process((ByteBuffer) k.rewind().limit(kb), (ByteBuffer) v.rewind().limit(vb)));
     }
 
-    public void getAll(GetAllByteArrayCallback callback) {
-        database_get_all_bytes(pointer, callback);
-    }
-
-    public void getAll(GetAllStringCallback callback) {
-        database_get_all_string(pointer, callback);
-    }
-
     public void getAbove(ByteBuffer key, GetAllBufferCallback callback) {
         database_get_above_buffer(pointer, key.position(), key, (int kb, ByteBuffer k, int vb, ByteBuffer v)
                 -> callback.process((ByteBuffer) k.rewind().limit(kb), (ByteBuffer) v.rewind().limit(vb)));
-    }
-
-    public void getAbove(byte[] key, GetAllByteArrayCallback callback) {
-        database_get_above_bytes(pointer, key, callback);
-    }
-
-    public void getAbove(String key, GetAllStringCallback callback) {
-        database_get_above_string(pointer, key.getBytes(), callback);
     }
 
     public void getBelow(ByteBuffer key, GetAllBufferCallback callback) {
@@ -148,37 +76,13 @@ public class Database {
                 -> callback.process((ByteBuffer) k.rewind().limit(kb), (ByteBuffer) v.rewind().limit(vb)));
     }
 
-    public void getBelow(byte[] key, GetAllByteArrayCallback callback) {
-        database_get_below_bytes(pointer, key, callback);
-    }
-
-    public void getBelow(String key, GetAllStringCallback callback) {
-        database_get_below_string(pointer, key.getBytes(), callback);
-    }
-
     public void getBetween(ByteBuffer key1, ByteBuffer key2, GetAllBufferCallback callback) {
         database_get_between_buffer(pointer, key1.position(), key1, key2.position(), key2, (int kb, ByteBuffer k, int vb, ByteBuffer v)
                 -> callback.process((ByteBuffer) k.rewind().limit(kb), (ByteBuffer) v.rewind().limit(vb)));
     }
 
-    public void getBetween(byte[] key1, byte[] key2, GetAllByteArrayCallback callback) {
-        database_get_between_bytes(pointer, key1, key2, callback);
-    }
-
-    public void getBetween(String key1, String key2, GetAllStringCallback callback) {
-        database_get_between_string(pointer, key1.getBytes(), key2.getBytes(), callback);
-    }
-
     public boolean exists(ByteBuffer key) {
         return database_exists_buffer(pointer, key.position(), key);
-    }
-
-    public boolean exists(byte[] key) {
-        return database_exists_bytes(pointer, key);
-    }
-
-    public boolean exists(String key) {
-        return database_exists_bytes(pointer, key.getBytes());
     }
 
     public void get(ByteBuffer key, GetKeysBuffersCallback callback) {
@@ -210,45 +114,9 @@ public class Database {
         }
     }
 
-    public void put(byte[] key, byte[] value) {
-        try {
-            database_put_bytes(pointer, key, value);
-        } catch (DatabaseException kve) {
-            kve.setKey(key);
-            throw kve;
-        }
-    }
-
-    public void put(String key, String value) {
-        try {
-            database_put_bytes(pointer, key.getBytes(), value.getBytes());
-        } catch (DatabaseException kve) {
-            kve.setKey(key);
-            throw kve;
-        }
-    }
-
     public boolean remove(ByteBuffer key) {
         try {
             return database_remove_buffer(pointer, key.position(), key);
-        } catch (DatabaseException kve) {
-            kve.setKey(key);
-            throw kve;
-        }
-    }
-
-    public boolean remove(byte[] key) {
-        try {
-            return database_remove_bytes(pointer, key);
-        } catch (DatabaseException kve) {
-            kve.setKey(key);
-            throw kve;
-        }
-    }
-
-    public boolean remove(String key) {
-        try {
-            return database_remove_bytes(pointer, key.getBytes());
         } catch (DatabaseException kve) {
             kve.setKey(key);
             throw kve;
@@ -263,45 +131,23 @@ public class Database {
     private native long database_start(String engine, String config);
     private native void database_stop(long ptr);
     private native void database_get_keys_buffer(long ptr, GetKeysBuffersJNICallback cb);
-    private native void database_get_keys_bytes(long ptr, GetKeysByteArraysCallback cb);
-    private native void database_get_keys_string(long ptr, GetKeysStringsCallback cb);
     private native void database_get_keys_above_buffer(long ptr, int kb, ByteBuffer k, GetKeysBuffersJNICallback cb);
-    private native void database_get_keys_above_bytes(long ptr, byte[] k, GetKeysByteArraysCallback cb);
-    private native void database_get_keys_above_string(long ptr, byte[] k, GetKeysStringsCallback cb);
     private native void database_get_keys_below_buffer(long ptr, int kb, ByteBuffer k, GetKeysBuffersJNICallback cb);
-    private native void database_get_keys_below_bytes(long ptr, byte[] k, GetKeysByteArraysCallback cb);
-    private native void database_get_keys_below_string(long ptr, byte[] k, GetKeysStringsCallback cb);
     private native void database_get_keys_between_buffer(long ptr, int kb1, ByteBuffer k1, int kb2, ByteBuffer k2, GetKeysBuffersJNICallback cb);
-    private native void database_get_keys_between_bytes(long ptr, byte[] k1, byte[] k2, GetKeysByteArraysCallback cb);
-    private native void database_get_keys_between_string(long ptr, byte[] k1, byte[] k2, GetKeysStringsCallback cb);
     private native long database_count_all(long ptr);
     private native long database_count_above_buffer(long ptr, int kb, ByteBuffer k);
-    private native long database_count_above_bytes(long ptr, byte[] k);
     private native long database_count_below_buffer(long ptr, int kb, ByteBuffer k);
-    private native long database_count_below_bytes(long ptr, byte[] k);
     private native long database_count_between_buffer(long ptr, int kb1, ByteBuffer k1, int kb2, ByteBuffer k2);
-    private native long database_count_between_bytes(long ptr, byte[] k1, byte[] k2);
     private native void database_get_all_buffer(long ptr, GetAllBufferJNICallback cb);
-    private native void database_get_all_bytes(long ptr, GetAllByteArrayCallback cb);
-    private native void database_get_all_string(long ptr, GetAllStringCallback cb);
     private native void database_get_above_buffer(long ptr, int kb, ByteBuffer k, GetAllBufferJNICallback cb);
-    private native void database_get_above_bytes(long ptr, byte[] k, GetAllByteArrayCallback cb);
-    private native void database_get_above_string(long ptr, byte[] k, GetAllStringCallback cb);
     private native void database_get_below_buffer(long ptr, int kb, ByteBuffer k, GetAllBufferJNICallback cb);
-    private native void database_get_below_bytes(long ptr, byte[] k, GetAllByteArrayCallback cb);
-    private native void database_get_below_string(long ptr, byte[] k, GetAllStringCallback cb);
     private native void database_get_between_buffer(long ptr, int kb1, ByteBuffer k1, int kb2, ByteBuffer k2, GetAllBufferJNICallback cb);
-    private native void database_get_between_bytes(long ptr, byte[] k1, byte[] k2, GetAllByteArrayCallback cb);
-    private native void database_get_between_string(long ptr, byte[] k1, byte[] k2, GetAllStringCallback cb);
     private native boolean database_exists_buffer(long ptr, int kb, ByteBuffer k);
-    private native boolean database_exists_bytes(long ptr, byte[] k);
     private native int database_get_buffer(long ptr, int kb, ByteBuffer k, int vb, ByteBuffer v);
     private native void database_get_buffer_with_callback(long ptr, int kb, ByteBuffer k, GetKeysBuffersJNICallback cb);
     private native byte[] database_get_bytes(long ptr, byte[] k);
     private native void database_put_buffer(long ptr, int kb, ByteBuffer k, int vb, ByteBuffer v);
-    private native void database_put_bytes(long ptr, byte[] k, byte[] v);
     private native boolean database_remove_buffer(long ptr, int kb, ByteBuffer k);
-    private native boolean database_remove_bytes(long ptr, byte[] k);
 
     static {
         System.loadLibrary("pmemkv-jni");
