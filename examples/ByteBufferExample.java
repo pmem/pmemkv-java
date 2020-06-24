80736b8de6b1ec319d33d8da802ca9fa107b637b
Pawel Karczewski <pawel.karczewski@intel.com>
2020-06-24 11:07:18 +0200

    Add example for ByteBuffer

---
+++ examples/ByteBufferExample.java
@@ -0,0 +1,44 @@
+import io.pmem.pmemkv.Database;
+import io.pmem.pmemkv.Converter;
+
+import java.nio.ByteBuffer;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+class ByteBufferConverter implements Converter<ByteBuffer> {
+    public ByteBuffer toByteBuffer(ByteBuffer entry) {
+      return entry;
+    }
+
+    public ByteBuffer fromByteBuffer(ByteBuffer entry) {
+      return entry;
+    }
+}
+
+public class ByteBufferExample {
+
+    public static void main(String[] args) {
+         String ENGINE = "vsmap";
+         String CONFIG = "{\"path\":\"/dev/shm\", \"size\":1073741824}";
+
+        Database<ByteBuffer, ByteBuffer> db = new Database.Builder(ENGINE).
+                setSize(1073741824).
+                setPath("/dev/shm").
+                setKeyConverter(new ByteBufferConverter()).
+                setValueConverter(new ByteBufferConverter()).
+                <ByteBuffer, ByteBuffer>build();
+
+        // Direct ByteBuffer
+        for ( int i = 0; i< 0xFF; i++){
+            ByteBuffer key = ByteBuffer.allocateDirect(4);
+            key.putInt(i);
+            db.put(key, key);
+        }
+        db.getAll((ByteBuffer k, ByteBuffer v) -> {
+            System.out.println("Key: " + String.format("0x%02X", k.getInt()) +
+                    " Value: " +  String.format("0x%02X", v.getInt()));
+        });
+
+        db.stop();
+
+    }
+}
