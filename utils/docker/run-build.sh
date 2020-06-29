680d8567c33587ee8ac0e9ce4edd07d70398a35f
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2020-06-29 16:49:51 +0200

    compile and run examples in CI

--- utils/docker/run-build.sh
+++ utils/docker/run-build.sh
@@ -11,6 +11,14 @@ set -e
 
 source `dirname $0`/prepare-for-build.sh
 
+function run_example() {
+	example_name=$1
+	jar_path=../src/main/target/pmemkv-1.0.0.jar
+
+	javac -g -cp ${jar_path} ${example_name}.java
+	java -ea -Xms1G -cp .:${jar_path} ${example_name}
+}
+
 # install pmemkv
 pmemkv_version=$1
 cd /opt/pmemkv-$pmemkv_version/
@@ -25,9 +33,20 @@ fi
 
 echo
 echo "###########################################################"
-echo "### Verifying building and installing of the java bindings "
+echo "### Verifying building and installing of the java bindings"
 echo "###########################################################"
 cd $WORKDIR
 mkdir -p ~/.m2/repository
 cp -r /opt/java/repository ~/.m2/
 mvn install -e
+
+echo
+echo "###########################################################"
+echo "### Verifying building and execution of examples"
+echo "###########################################################"
+# set path to libpmemkv-jni.so
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORKDIR/src/main/cpp/target
+cd examples
+run_example StringExample
+run_example ByteBufferExample
+run_example MixedTypesExample
