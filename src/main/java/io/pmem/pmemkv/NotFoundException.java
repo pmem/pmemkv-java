61b0db62a2556d98a36031bff6668efeb60b7025
Pawel Karczewski <pawel.karczewski@intel.com>
2020-06-29 14:58:19 +0200

    Add javadoc for exceptions
    Descriptions copied form pmemkv-python

--- src/main/java/io/pmem/pmemkv/NotFoundException.java
+++ src/main/java/io/pmem/pmemkv/NotFoundException.java
@@ -3,6 +3,12 @@
 
 package io.pmem.pmemkv;
 
+/**
+ * Database entry or config item not found
+ *
+ * @see <a href="https://pmem.io/pmemkv/master/manpages/libpmemkv.3.html#errors">Pmemkv errors</a>
+ */
+
 public class NotFoundException extends DatabaseException {
 
     public NotFoundException(String message) {
