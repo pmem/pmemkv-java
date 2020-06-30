31d6157a90ccc8fb4b827437c58665c9e162bdd7
Pawel Karczewski <pawel.karczewski@intel.com>
2020-06-30 19:06:06 +0200

    Add javadoc and mark as FunctionalInterface callback interfaces

--- src/main/java/io/pmem/pmemkv/KeyCallback.java
+++ src/main/java/io/pmem/pmemkv/KeyCallback.java
@@ -3,8 +3,21 @@
 
 package io.pmem.pmemkv;
 
+/**
+ * Represents callback function, which handle value-only use cases. Such
+ * expression may be passed to get() method in Database class
+ *
+ * @param <KeyT>
+ *            the type of a key stored in the pmemkv database
+ */
+@FunctionalInterface
 public interface KeyCallback<KeyT> {
-
+	/**
+	 * It's internally used as a middle layer to run callback function
+	 *
+	 * @param key
+	 *            the key returned by pmemkv engine to callback function
+	 */
 	void process(KeyT key);
 
 }
