6bbb4ac6cabdcc096be0dfc249db03b52599b64a
Krzysztof Filipek <krzysztof.filipek@intel.com>
2020-12-10 17:51:47 +0100

    project reorganization: avoid nested projects
    - keep flat tree
    - target separation
    - .so file included into jar file
    - compile executable jar per example (deps and no-deps included)
    - readme files update

--- pmemkv-binding/src/main/java/io/pmem/pmemkv/internal/GetKeysBuffersJNICallback.java
+++ pmemkv-binding/src/main/java/io/pmem/pmemkv/internal/GetKeysBuffersJNICallback.java
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2017-2019, Intel Corporation */
+// Copyright 2017-2019, Intel Corporation
 
 package io.pmem.pmemkv.internal;
 
 import java.nio.ByteBuffer;
 
 public interface GetKeysBuffersJNICallback {
-
 	void process(int keybytes, ByteBuffer key);
-
 }
