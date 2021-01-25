265b9e458717622e2578f8136e497f39218de7e4
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-01-25 15:28:59 +0100

    CI: use GitHub Container registry instead of DockerHub

--- utils/docker/images/push-image.sh
+++ utils/docker/images/push-image.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2020, Intel Corporation
+# Copyright 2016-2021, Intel Corporation
 
 #
-# push-image.sh - pushes the Docker image tagged with OS-VER to the Docker Hub.
+# push-image.sh - pushes the Docker image tagged with OS-VER to the ${CONTAINER_REG}.
 #
-# The script utilizes $DOCKERHUB_USER and $DOCKERHUB_PASSWORD variables to
-# log in to the Docker Hub. The variables can be set in the CI's configuration
-# for automated builds.
+# The script utilizes ${CONTAINER_REG_USER} and ${CONTAINER_REG_PASS} variables to
+# log in to the ${CONTAINER_REG}.
 #
 
 set -e
@@ -22,23 +21,32 @@ if [[ -z "$OS_VER" ]]; then
 	exit 1
 fi
 
-if [[ -z "${DOCKERHUB_REPO}" ]]; then
-	echo "DOCKERHUB_REPO environment variable is not set"
+if [[ -z "${CONTAINER_REG}" ]]; then
+	echo "ERROR: CONTAINER_REG environment variable is not set " \
+		"(e.g. \"<registry_addr>/<org_name>/<package_name>\")."
+	exit 1
+fi
+
+if [[ -z "${CONTAINER_REG_USER}" || -z "${CONTAINER_REG_PASS}" ]]; then
+	echo "ERROR: variables CONTAINER_REG_USER=\"${CONTAINER_REG_USER}\" " \
+		"and CONTAINER_REG_PASS=\"${CONTAINER_REG_PASS}\"" \
+		"have to be set properly to allow login to the Container Registry."
 	exit 1
 fi
 
 TAG="1.1-${OS}-${OS_VER}"
 
-# Check if the image tagged with ${DOCKERHUB_REPO}:${TAG} exists locally
-if [[ ! $(docker images -a | awk -v pattern="^${DOCKERHUB_REPO}:${TAG}\$" \
+echo "Check if the image tagged with ${CONTAINER_REG}:${TAG} exists locally"
+if [[ ! $(docker images -a | awk -v pattern="^${CONTAINER_REG}:${TAG}\$" \
 	'$1":"$2 ~ pattern') ]]
 then
-	echo "ERROR: Docker image tagged ${DOCKERHUB_REPO}:${TAG} does not exist locally."
+	echo "ERROR: Docker image tagged ${CONTAINER_REG}:${TAG} does not exist locally."
 	exit 1
 fi
 
-# Log in to the Docker Hub
-docker login -u="$DOCKERHUB_USER" -p="$DOCKERHUB_PASSWORD"
+echo "Log in to the Container Registry: ${CONTAINER_REG}"
+echo "${CONTAINER_REG_PASS}" | docker login "${CONTAINER_REG}" -u="${CONTAINER_REG_USER}" --password-stdin
 
-# Push the image to the repository
-docker push ${DOCKERHUB_REPO}:${TAG}
+echo "Push the image '${CONTAINER_REG}:${TAG}' to the Container Registry."
+docker push ${CONTAINER_REG}:${TAG}
+echo "Image pushed."
