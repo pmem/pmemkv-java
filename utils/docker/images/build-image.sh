ecb9cba474ff3517906e67633f137d8f2e28e373
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-01-26 10:12:21 +0100

    utils: use env var OS and VER instead of build-image.sh given params

--- utils/docker/images/build-image.sh
+++ utils/docker/images/build-image.sh
@@ -3,7 +3,7 @@
 # Copyright 2016-2021, Intel Corporation
 
 #
-# build-image.sh <OS-OS_VER> - prepares a Docker image with <OS>-based
+# build-image.sh - prepares a Docker image with <OS>-based
 #                           environment for testing pmemkv-java, according
 #                           to the Dockerfile.<OS-OS_VER> file located
 #                           in the same directory.
@@ -12,21 +12,11 @@
 #
 
 set -e
+TAG="1.1-${OS}-${OS_VER}"
 
-OS__OS_VER=$1
-TAG="1.1-${OS__OS_VER}"
-
-function usage {
-	echo "Usage:"
-	echo "    build-image.sh <OS-OS_VER>"
-	echo "where <OS-OS_VER>, for example, can be 'fedora-32', provided " \
-		"a Dockerfile named 'Dockerfile.fedora-32' exists in the " \
-		"current directory."
-}
-
-# Check if the argument is not empty
-if [[ -z "$1" ]]; then
-	usage
+if [[ -z "${OS}" || -z "${OS_VER}" ]]; then
+	echo "ERROR: The variables OS and OS_VER have to be set " \
+		"(e.g. OS=fedora, OS_VER=32)."
 	exit 1
 fi
 
@@ -36,16 +26,14 @@ if [[ -z "${CONTAINER_REG}" ]]; then
 	exit 1
 fi
 
-# Check if the file Dockerfile.OS-OS_VER exists
-if [[ ! -f "Dockerfile.$OS__OS_VER" ]]; then
-	echo "Error: Dockerfile.$OS__OS_VER does not exist."
-	echo
-	usage
+echo "Check if the file Dockerfile.${OS}-${OS_VER} exists"
+if [[ ! -f "Dockerfile.${OS}-${OS_VER}" ]]; then
+	echo "Error: Dockerfile.${OS}-${OS_VER} does not exist."
 	exit 1
 fi
 
 echo "Build a Docker image tagged with: ${CONTAINER_REG}:${TAG}"
 docker build -t ${CONTAINER_REG}:${TAG} \
-	--build-arg http_proxy=$http_proxy \
-	--build-arg https_proxy=$https_proxy \
-	-f Dockerfile.${OS__OS_VER} .
+	--build-arg http_proxy=${http_proxy} \
+	--build-arg https_proxy=${https_proxy} \
+	-f Dockerfile.${OS}-${OS_VER} .
