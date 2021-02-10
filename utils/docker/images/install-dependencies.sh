588b7c23bf1ac8f5f474d3c449c25b80b1872836
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-02-10 14:51:46 +0100

    docker: cleanup install-dependencies.sh

--- utils/docker/images/install-dependencies.sh
+++ utils/docker/images/install-dependencies.sh
@@ -3,8 +3,8 @@
 # Copyright 2019-2021, Intel Corporation
 
 #
-# install-dependencies.sh - install Java dependencies
-#                           so that it can be built offline
+# install-dependencies.sh - install binding's dependencies
+#                           so it can be built offline
 #
 
 set -e
@@ -22,17 +22,15 @@ if [ "${SKIP_PMDK_BUILD}" ]; then
 	exit
 fi
 
-#
-# Build and install PMEMKV - JNI will need it
-#
+echo "Build and install PMEMKV (JNI needs it)"
 git clone https://github.com/pmem/pmemkv.git
 cd pmemkv
-git checkout $PMEMKV_VERSION
+git checkout ${PMEMKV_VERSION}
 mkdir build
 cd build
 
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-	-DCMAKE_INSTALL_PREFIX=$PREFIX \
+	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	-DENGINE_CMAP=OFF \
 	-DENGINE_CSMAP=OFF \
 	-DENGINE_VCMAP=OFF \
@@ -47,27 +45,27 @@ make -j$(nproc) install
 # project's dependencies - all of the dependencies needed to run pmemkv-java will
 #                          be saved in the /opt/java directory. It makes building
 #                          of this project independent of network connection.
+echo "Save binding's dependencies in /opt/java"
 mkdir /opt/java/
 
 deps_dir=$(mktemp -d)
 git clone https://github.com/pmem/pmemkv-java.git ${deps_dir}
 pushd ${deps_dir}
-git checkout $JAVA_VERSION
+git checkout ${JAVA_VERSION}
 mvn install -Dmaven.test.skip=true
-mvn javadoc:javadoc -e
+mvn javadoc:javadoc
 mvn dependency:go-offline
 mv -v ~/.m2/repository /opt/java/
 
 popd
 rm -r ${deps_dir}
 
-#
-# Uninstall all installed stuff
-#
-cd $WORKDIR/pmemkv/build
+echo "Uninstall pmemkv"
+cd ${WORKDIR}/pmemkv/build
 make uninstall
-# remove any installed pmemkv-java's libs
+
+echo "Remove installed binding files (from local mvn repository)"
 rm -r /opt/java/repository/io/pmem/*
 
-# make the /opt/java directory world-readable
+echo "Make the /opt/java directory world-readable"
 chmod -R a+r /opt/java
