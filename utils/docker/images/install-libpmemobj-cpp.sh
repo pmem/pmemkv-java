19af8ef1be82adb934a1f03d4f22a2cce57f988b
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-03-08 11:23:37 +0100

    docker: update dependencies' scripts
    and bump pmdk, libpmemobj-cpp and pmemkv versions.

--- utils/docker/images/install-libpmemobj-cpp.sh
+++ utils/docker/images/install-libpmemobj-cpp.sh
@@ -3,44 +3,49 @@
 # Copyright 2019-2021, Intel Corporation
 
 #
-# install-libpmemobj-cpp.sh <package_type>
+# install-libpmemobj-cpp.sh [package_type]
 #			- installs PMDK C++ bindings (libpmemobj-cpp)
 #
 
 set -e
-
-PREFIX=/usr
-PACKAGE_TYPE=$1
-
-# master: Merge pull request #991 from igchor/merge_1.11_into_master; 10.12.2020
-LIBPMEMOBJ_CPP_VERSION="1277d53c15a0e69ba7af2912818c72f2cb7ac708"
 
 if [ "${SKIP_LIBPMEMOBJ_CPP_BUILD}" ]; then
 	echo "Variable 'SKIP_LIBPMEMOBJ_CPP_BUILD' is set; skipping building of libpmemobj-cpp"
 	exit
 fi
 
-git clone https://github.com/pmem/libpmemobj-cpp --shallow-since=2020-04-28
-cd libpmemobj-cpp
-git checkout $LIBPMEMOBJ_CPP_VERSION
+PREFIX=/usr
+PACKAGE_TYPE=${1^^} #To uppercase
+echo "PACKAGE_TYPE: ${PACKAGE_TYPE}"
+
+# Merge pull request #1048 from lukaszstolarczuk/tests-gdb; 04.03.2021
+# It contains new exception: pool_invalid_argument
+LIBPMEMOBJ_CPP_VERSION="7cc2f387fa261f370a3c5f0f9e122756bc2fffb0"
+
+build_dir=$(mktemp -d -t libpmemobj-cpp-XXX)
+
+git clone https://github.com/pmem/libpmemobj-cpp --shallow-since=2020-06-01 ${build_dir}
+
+pushd ${build_dir}
+git checkout ${LIBPMEMOBJ_CPP_VERSION}
 
 mkdir build
 cd build
 
-cmake .. -DCPACK_GENERATOR="$PACKAGE_TYPE" \
-	-DCMAKE_INSTALL_PREFIX=$PREFIX \
-	-DCMAKE_BUILD_TYPE=RelWithDebInfo
+# turn off all redundant components
+cmake .. -DCPACK_GENERATOR="${PACKAGE_TYPE}" -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+	-DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_DOC=OFF -DBUILD_BENCHMARKS=OFF
 
-if [ "$PACKAGE_TYPE" = "" ]; then
+if [ "${PACKAGE_TYPE}" = "" ]; then
 	make -j$(nproc) install
 else
 	make -j$(nproc) package
-	if [ "$PACKAGE_TYPE" = "DEB" ]; then
+	if [ "${PACKAGE_TYPE}" = "DEB" ]; then
 		sudo dpkg -i libpmemobj++*.deb
-	elif [ "$PACKAGE_TYPE" = "RPM" ]; then
+	elif [ "${PACKAGE_TYPE}" = "RPM" ]; then
 		sudo rpm -i libpmemobj++*.rpm
 	fi
 fi
 
-cd ../..
-rm -r libpmemobj-cpp
+popd
+rm -r ${build_dir}
--- utils/docker/images/install-pmdk.sh
+++ utils/docker/images/install-pmdk.sh
@@ -3,34 +3,35 @@
 # Copyright 2019-2021, Intel Corporation
 
 #
-# install-pmdk.sh <package_type> - installs PMDK
+# install-pmdk.sh [package_type] - installs PMDK
 #
 
 set -e
-
-PREFIX=/usr
-PACKAGE_TYPE=$1
-
-# master: 1.9, 03.07.2020
-PMDK_VERSION="1.9"
 
 if [ "${SKIP_PMDK_BUILD}" ]; then
 	echo "Variable 'SKIP_PMDK_BUILD' is set; skipping building of PMDK"
 	exit
 fi
 
-git clone https://github.com/pmem/pmdk --shallow-since=2020-01-15
-cd pmdk
-git checkout $PMDK_VERSION
+PREFIX=/usr
+PACKAGE_TYPE=$1
+echo "PACKAGE_TYPE: ${PACKAGE_TYPE}"
 
-if [ "$PACKAGE_TYPE" = "" ]; then
+# master: 1.9.1, 16.09.2020
+PMDK_VERSION="c47fd17daaeee3ab475d87aad70bcf751bb189ef"
+
+git clone https://github.com/pmem/pmdk --shallow-since=2020-06-01
+cd pmdk
+git checkout ${PMDK_VERSION}
+
+if [ "${PACKAGE_TYPE}" = "" ]; then
 	make -j$(nproc) install prefix=$PREFIX
 else
-	make -j$(nproc) BUILD_PACKAGE_CHECK=n $PACKAGE_TYPE
-	if [ "$PACKAGE_TYPE" = "dpkg" ]; then
+	make -j$(nproc) BUILD_PACKAGE_CHECK=n ${PACKAGE_TYPE}
+	if [ "${PACKAGE_TYPE}" = "dpkg" ]; then
 		sudo dpkg -i dpkg/libpmem_*.deb dpkg/libpmem-dev_*.deb
 		sudo dpkg -i dpkg/libpmemobj_*.deb dpkg/libpmemobj-dev_*.deb
-	elif [ "$PACKAGE_TYPE" = "rpm" ]; then
+	elif [ "${PACKAGE_TYPE}" = "rpm" ]; then
 		sudo rpm -i rpm/*/pmdk-debuginfo-*.rpm
 		sudo rpm -i rpm/*/libpmem-*.rpm
 		sudo rpm -i rpm/*/libpmemobj-*.rpm
