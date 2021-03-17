327518b6ecd6663ad239a8df040ca9bc8f48f584
Jan Dorniak <jan.dorniak@intel.com>
2021-03-17 10:54:17 +0100

    add caching of direct buffer and optimize calling callback from JNI

--- pmemkv-binding/src/test/java/io/pmem/pmemkv/CmapTest.java
+++ pmemkv-binding/src/test/java/io/pmem/pmemkv/CmapTest.java
@@ -87,4 +87,89 @@ public class CmapTest {
 
 		db.stop();
 	}
+
+	/* Test using multiple instances of the one DB */
+	@Test
+	public void multipleDBInstancesTest() {
+		final int threadsNumber = 8;
+		final int numberOfElements = 50;
+
+		String file = folder.getRoot() + File.pathSeparator + "testfile";
+		Database<ByteBuffer, ByteBuffer> db = createDB(ENGINE, file, new ByteBufferConverter());
+		db.stop();
+
+		runParallel(threadsNumber, () -> {
+			Database<ByteBuffer, ByteBuffer> threadDB = openDB(ENGINE, file, new ByteBufferConverter());
+			for (int j = 0; j < numberOfElements; ++j) {
+				final int x = j;
+				threadDB.put(stringToByteBuffer(Integer.toString(x)),
+						stringToByteBuffer(Integer.toString(x + 1)));
+			}
+			threadDB.stop();
+		});
+
+		runParallel(threadsNumber, () -> {
+			Database<ByteBuffer, ByteBuffer> threadDB = openDB(ENGINE, file, new ByteBufferConverter());
+			for (int j = 0; j < numberOfElements; ++j) {
+				final int x = j;
+				threadDB.exists(stringToByteBuffer(Integer.toString(x)));
+			}
+			threadDB.stop();
+		});
+
+		runParallel(threadsNumber, () -> {
+			Database<ByteBuffer, ByteBuffer> threadDB = openDB(ENGINE, file, new ByteBufferConverter());
+			for (int j = numberOfElements - 1; j >= 0; --j) {
+				final int x = j;
+				threadDB.get(stringToByteBuffer(Integer.toString(x)), (ByteBuffer v) -> {
+					assertEquals(byteBufferToString(v), Integer.toString(x + 1));
+				});
+			}
+			threadDB.stop();
+		});
+	}
+
+	/* Test using multiple instances of 2 different DBs */
+	@Test
+	public void multipleDBTypesTest() {
+		final int threadsNumber = 4;
+		final int numberOfElements = 50;
+
+		String file1 = folder.getRoot() + File.pathSeparator + "testfile1";
+		Database<String, String> dbString = createDB(ENGINE, file1, new StringConverter(), 10, 100);
+		final StringBuilder sb = new StringBuilder(numberOfElements + 1);
+		sb.append('x');
+		for (int i = 0; i < numberOfElements; ++i) {
+			sb.append('x');
+			dbString.put(sb.substring(0, i), sb.substring(0, i + 1));
+		}
+		dbString.stop();
+
+		String file2 = folder.getRoot() + File.pathSeparator + "testfile2";
+		Database<ByteBuffer, ByteBuffer> dbByteBuffer = createDB(ENGINE, file2, new ByteBufferConverter(), 20, 200);
+		for (int i = 0; i < numberOfElements; ++i) {
+			dbByteBuffer.put(stringToByteBuffer(sb.substring(0, i)), stringToByteBuffer(sb.substring(0, i + 1)));
+		}
+		dbByteBuffer.stop();
+
+		runParallel(threadsNumber, () -> {
+			Database<String, String> threadDB = openDB(ENGINE, file1, new StringConverter());
+			for (int j = 0; j < numberOfElements; ++j) {
+				final int x = j;
+				threadDB.get(sb.substring(0, x), (String v) -> {
+					assertEquals(v, sb.substring(0, x + 1));
+				});
+			}
+			threadDB.stop();
+		}, () -> {
+			Database<ByteBuffer, ByteBuffer> threadDB = openDB(ENGINE, file2, new ByteBufferConverter());
+			for (int j = 0; j < numberOfElements; ++j) {
+				final int x = j;
+				threadDB.get(stringToByteBuffer(sb.substring(0, x)), (ByteBuffer v) -> {
+					assertEquals(byteBufferToString(v), sb.substring(0, x + 1));
+				});
+			}
+			threadDB.stop();
+		});
+	}
 }
