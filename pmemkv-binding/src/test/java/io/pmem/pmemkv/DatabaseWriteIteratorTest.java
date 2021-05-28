47951b622e3ec4e043e5a4560d35f9b5544138a1
Krzysztof Filipek <krzysztof.filipek@intel.com>
2021-05-28 16:11:51 +0200

    [Java] Implement seek* functions for Iterator API
    Exposes seek* iterator C++ API to Java.

--- pmemkv-binding/src/test/java/io/pmem/pmemkv/DatabaseWriteIteratorTest.java
+++ pmemkv-binding/src/test/java/io/pmem/pmemkv/DatabaseWriteIteratorTest.java
@@ -21,12 +21,20 @@ public class DatabaseWriteIteratorTest {
 	private final String ENGINE = "vsmap";
 	private String DB_DIR = "";
 
-	/* Helper method, used in most of the tests in this file */
+	/* Helper methods, used in most of the tests in this file */
 	private Database<ByteBuffer, ByteBuffer> buildDB(String engine) {
 		Database<ByteBuffer, ByteBuffer> db = openDB(engine, DB_DIR, new ByteBufferConverter());
 		assertNotNull(db);
 		assertFalse(db.stopped());
 		return db;
+	}
+
+	private TreeMap<String, String> buildHashMap(int size) {
+		TreeMap<String, String> hs = new TreeMap<String, String>();
+		for (int i = 0; i < 10; i++) {
+			hs.put("key" + i, "value" + i);
+		}
+		return hs;
 	}
 
 	@Rule
@@ -45,10 +53,11 @@ public class DatabaseWriteIteratorTest {
 		assertFalse(db.exists(stringToByteBuffer("key1")));
 		db.put(stringToByteBuffer("key1"), stringToByteBuffer("value1"));
 		assertTrue(db.exists(stringToByteBuffer("key1")));
-		Database<ByteBuffer, ByteBuffer>.WriteIterator it = db.iterator();
-		assertNotNull(it);
-		assertTrue(it.seekToFirst());;
-		assertTrue(byteBufferToString(it.key()).equals("key1"));
+		try (Database<ByteBuffer, ByteBuffer>.WriteIterator it = db.iterator()) {
+			assertNotNull(it);
+			assertTrue(it.seekToFirst());;
+			assertTrue(byteBufferToString(it.key()).equals("key1"));
+		}
 		db.stop();
 		assertTrue(db.stopped());
 	}
@@ -67,17 +76,122 @@ public class DatabaseWriteIteratorTest {
 			db.put(stringToByteBuffer(entry.getKey()), stringToByteBuffer(entry.getValue()));
 		}
 
-		Database<ByteBuffer, ByteBuffer>.WriteIterator it = db.iterator();
-		assertNotNull(it);
-		assertTrue(it.seekToFirst());
-		assertTrue(it.isNext());
-		int counter = 1;
-		while (it.isNext()) {
-			counter++;
-			assertTrue(it.next());;
+		try (Database<ByteBuffer, ByteBuffer>.WriteIterator it = db.iterator()) {
+			assertNotNull(it);
+			assertTrue(it.seekToFirst());
+			assertTrue(it.isNext());
+			int counter = 1;
+			while (it.isNext()) {
+				counter++;
+				assertTrue(it.next());;
+			}
+			assertTrue(counter == hs.size());
 		}
-		assertTrue(counter == hs.size());
 		db.stop();
 		assertTrue(db.stopped());
+	}
+
+	@Test
+	public void seekAndReadKeyTest() {
+		TreeMap<String, String> hs = buildHashMap(10);
+		Database<ByteBuffer, ByteBuffer> db = buildDB(ENGINE);
+
+		for (Map.Entry<String, String> entry : hs.entrySet()) {
+			db.put(stringToByteBuffer(entry.getKey()), stringToByteBuffer(entry.getValue()));
+		}
+
+		try (Database<ByteBuffer, ByteBuffer>.WriteIterator it = db.iterator()) {
+			assertTrue(it.seek(stringToByteBuffer("key3")));
+			assertTrue(byteBufferToString(it.key()).equals("key3"));
+			while (it.isNext()) {
+				assertTrue(it.next());
+				assertTrue(hs.containsKey(byteBufferToString(it.key())));
+			}
+		}
+		db.stop();
+	}
+
+	@Test
+	public void seekLowerAndReadKeyTest() {
+		TreeMap<String, String> hs = buildHashMap(10);
+		Database<ByteBuffer, ByteBuffer> db = buildDB(ENGINE);
+
+		for (Map.Entry<String, String> entry : hs.entrySet()) {
+			db.put(stringToByteBuffer(entry.getKey()), stringToByteBuffer(entry.getValue()));
+		}
+
+		try (Database<ByteBuffer, ByteBuffer>.WriteIterator it = db.iterator()) {
+			assertTrue(it.seekLower(stringToByteBuffer("key3")));
+			if (ENGINE.equals("vsmap"))
+				assertTrue(byteBufferToString(it.key()).equals("key2"));
+			while (it.isNext()) {
+				assertTrue(it.next());
+				assertTrue(hs.containsKey(byteBufferToString(it.key())));
+			}
+		}
+		db.stop();
+	}
+
+	@Test
+	public void seekLowerEqAndReadKeyTest() {
+		TreeMap<String, String> hs = buildHashMap(10);
+		Database<ByteBuffer, ByteBuffer> db = buildDB(ENGINE);
+
+		for (Map.Entry<String, String> entry : hs.entrySet()) {
+			db.put(stringToByteBuffer(entry.getKey()), stringToByteBuffer(entry.getValue()));
+		}
+
+		try (Database<ByteBuffer, ByteBuffer>.WriteIterator it = db.iterator()) {
+			assertTrue(it.seekLowerEq(stringToByteBuffer("key3")));
+			if (ENGINE.equals("vsmap"))
+				assertTrue(byteBufferToString(it.key()).equals("key3"));
+			while (it.isNext()) {
+				assertTrue(it.next());
+				assertTrue(hs.containsKey(byteBufferToString(it.key())));
+			}
+		}
+		db.stop();
+	}
+
+	@Test
+	public void seekHigherAndReadKeyTest() {
+		TreeMap<String, String> hs = buildHashMap(10);
+		Database<ByteBuffer, ByteBuffer> db = buildDB(ENGINE);
+
+		for (Map.Entry<String, String> entry : hs.entrySet()) {
+			db.put(stringToByteBuffer(entry.getKey()), stringToByteBuffer(entry.getValue()));
+		}
+
+		try (Database<ByteBuffer, ByteBuffer>.WriteIterator it = db.iterator()) {
+			assertTrue(it.seekHigher(stringToByteBuffer("key3")));
+			if (ENGINE.equals("vsmap"))
+				assertTrue(byteBufferToString(it.key()).equals("key4"));
+			while (it.isNext()) {
+				assertTrue(it.next());
+				assertTrue(hs.containsKey(byteBufferToString(it.key())));
+			}
+		}
+		db.stop();
+	}
+
+	@Test
+	public void seekHigherEqAndReadKeyTest() {
+		TreeMap<String, String> hs = buildHashMap(10);
+		Database<ByteBuffer, ByteBuffer> db = buildDB(ENGINE);
+
+		for (Map.Entry<String, String> entry : hs.entrySet()) {
+			db.put(stringToByteBuffer(entry.getKey()), stringToByteBuffer(entry.getValue()));
+		}
+
+		try (Database<ByteBuffer, ByteBuffer>.WriteIterator it = db.iterator()) {
+			assertTrue(it.seekHigherEq(stringToByteBuffer("key3")));
+			if (ENGINE.equals("vsmap"))
+				assertTrue(byteBufferToString(it.key()).equals("key3"));
+			while (it.isNext()) {
+				assertTrue(it.next());
+				assertTrue(hs.containsKey(byteBufferToString(it.key())));
+			}
+		}
+		db.stop();
 	}
 }
