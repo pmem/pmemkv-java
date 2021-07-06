94d3a6992c04965c0d5e40c58f4c216b528e4bcb
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-07-06 17:12:38 +0200

    utils: cleanup old pmemkv versions

--- utils/docker/images/prepare-pmemkv.sh
+++ utils/docker/images/prepare-pmemkv.sh
@@ -24,18 +24,6 @@ fi
 # master: Merge pull request #991 from lukaszstolarczuk/update-dockers; 05.07.2021
 current_pmemkv_version="fd0f0f20989b1d8c75f54f3151542475a7da37d0"
 
-# stable-1.0: 1.0.3 release; 06.10.2020
-stable_1_pmemkv_version="77ae3ef23dc2b2db9c012dc343b125a785a0ffbc"
-
-# stable-1.1: 1.1 release; 31.01.2020
-stable_1_1_pmemkv_version="2f719305afb0f44103734851cfe825e1b1d73dbf"
-
-# stable-1.2: 1.2 release; 29.05.2020
-stable_1_2_pmemkv_version="1a9dccfd4b7c7437534838aaec7e5f3e38300dd6"
-
-# stable-1.3: 1.3 release; 02.10.2020
-stable_1_3_pmemkv_version="6f79229fd195310f4a45321e86e312e358fe481a"
-
 # stable-1.4: 1.4 release; 15.02.2021
 stable_1_4_pmemkv_version="ecb8fd65c5b07ed002d1018418ef809ab50d4e18"
 
@@ -59,10 +47,6 @@ git clone https://github.com/pmem/pmemkv
 cd pmemkv
 
 prepare_pmemkv "${current_pmemkv_version}" "pmemkv-master"
-prepare_pmemkv "${stable_1_pmemkv_version}" "pmemkv-stable-1.0"
-prepare_pmemkv "${stable_1_1_pmemkv_version}" "pmemkv-stable-1.1"
-prepare_pmemkv "${stable_1_2_pmemkv_version}" "pmemkv-stable-1.2"
-prepare_pmemkv "${stable_1_3_pmemkv_version}" "pmemkv-stable-1.3"
 prepare_pmemkv "${stable_1_4_pmemkv_version}" "pmemkv-stable-1.4"
 
 cd ..
